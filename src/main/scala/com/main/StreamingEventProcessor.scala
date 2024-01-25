c63c9c94f30039f0f8a4247022c4c68f4cbbd2ca
Chandan Kumar <gawri.chandan@gmail.com>
2024-01-25 07:44:07 +0530

    initial commit

---
+++ src/main/scala/com/main/StreamingEventProcessor.scala
@@ -0,0 +1,51 @@
+package com.main
+
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+object StreamingEventProcessor {
+
+  def main(args: Array[String]): Unit = {
+
+    // Create Spark session
+    val spark = SparkSession.builder
+      .appName("KafkaStreamReader")
+      .master("local[*]")
+      .getOrCreate()
+
+
+    val df = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9093")
+      .option("subscribe", "topic1")
+      .load()
+
+   df.printSchema()
+
+    /*val schema = new StructType()
+      .add("id", IntegerType)
+      .add("firstname", StringType)
+      .add("middlename", StringType)
+      .add("lastname", StringType)
+      .add("dob_year", IntegerType)
+      .add("dob_month", IntegerType)
+      .add("gender", StringType)
+      .add("salary", IntegerType)
+
+
+    val personStringDF = df.selectExpr("CAST(value AS STRING)")
+
+    val personDF = personStringDF.select(from_json(col("value"), schema).as("data"))
+      .select("data.*")*/
+
+    df.writeStream
+      .format("console")
+
+      .outputMode("append")
+      .start()
+      .awaitTermination()
+  }
+}
